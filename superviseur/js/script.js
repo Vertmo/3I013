6797c151b1ebee25d8bf2c59bf21def6abfd9556
Vertmo <basile.pesin@gmail.com>
2018-02-17 11:36:09 +0100

    Ajouté des tabs et pitit refactor

--- superviseur/js/script.js
+++ superviseur/js/script.js
@@ -1,30 +1,44 @@
 /**
  * Basic JS code, used to manage dynamic elements in the page
  */
+var active_page;
+
 $(function() {
     // Decorating the tabs
     $('#tabs .item').click(function() {
         $('#tabs .item').removeClass('active')
         $(this).addClass('active')
     })
+
+    // When clicking on "Résumé"
     $('#main-tab').click(function() {
-        $('#container-div').load('pages/main.html')
+        load_page('pages/main.html', null)
     })
+
+    // When clicking on "Timelines"
+    $('#timelines-tab').click(function() {
+        load_page('pages/timelines.html', null)
+    })
+
+    // When clicking on "Représentation circulaire"
+    $('#circular-rep-tab').click(function() {
+        load_page('pages/circular-rep.html', null)
+    })
+
+    // When clicking on "Représentaiton spatiale"
     $('#spatial-rep-tab').click(function() {
-        $('#container-div').load('pages/spatial-rep.html', function() {
-            var s1 = function(sketch) {
-                sketch.setup = function() {
-                    sketch.createCanvas(600, 400)
-                    sketch.background(0, 0, 0)
-                }
-            }
-            new p5(s1, 'spatial-sketch-holder')
+        load_page('pages/spatial-rep.html', function() {
+            new p5(spatialSketch, 'spatial-sketch-holder')
         })
     })
 
-    // Default tab
-    $('#container-div').load('pages/main.html')
+    // Default : main tab
+    load_page('pages/main.html', null)
 })
 
-function load_page(page) {
+function load_page(page, func) {
+    // If the page is already loaded, why reload it ?
+    if(active_page === page) return
+    $('#container-div').load(page, func)
+    active_page = page;
 }
---
+++ superviseur/js/sketches/spatial-rep.js
@@ -0,0 +1,13 @@
+/**
+ * Spatial representation of the classroom during a session
+ */
+
+function spatialSketch(sketch) {
+    /**
+     * Setup
+     */
+    sketch.setup = function() {
+        sketch.createCanvas(600, 400)
+        sketch.background(0, 0, 0)
+    }
+}
